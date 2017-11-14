76992ec7b211c4534b28870f3a25d6595544c75d
Tim Freeman <d.timothy.freeman@gmail.com>
2017-11-14 11:24:01 -0500

    mostly finished. restructured resume page without react-pdf. added link to full resume on goolge docs.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,41 +7,41 @@ const Projects = (props) => {
     <div className='main-content fade-in'>
       <div className='scroll-y'>
         <table>
-          <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Front End</th>
-            <th>Back End</th>
-            <th>Github</th>
-          </tr>
           <tbody>
             <tr>
-              <td><a href='https://evaluationmanager.herokuapp.com' target='_blank'><strong>eVALUEate</strong></a></td>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Front End</th>
+              <th>Back End</th>
+              <th>Github</th>
+            </tr>
+            <tr>
+              <td>eVALUEate<br/><a href='https://evaluationmanager.herokuapp.com' target='_blank' rel="noopener noreferrer"><strong className='small-text'>(Demo Site)</strong></a></td>
               <td>Employee evaluation management system. Companies can create, execute, and track data for their employee reviews.</td>
               <td>React, Redux, JavaScript, chart.js</td>
               <td>Ruby on Rails, Postgres</td>
-              <td className='no-border-right'><a href='https://github.com/dtfreemn/evaluator-react-redux' target='_blank'> <strong>Frontend</strong></a> <a href='https://github.com/dtfreemn/evaluator-rails' target='_blank'><strong>Backend</strong></a></td>
+              <td className='no-border-right'><a href='https://github.com/dtfreemn/evaluator-react-redux' target='_blank' rel="noopener noreferrer"> <strong>Frontend</strong></a> <a href='https://github.com/dtfreemn/evaluator-rails' target='_blank' rel="noopener noreferrer"><strong>Backend</strong></a></td>
             </tr>
             <tr>
-              <td><a href='https://moodboards-inc.herokuapp.com'><strong>MoodBoards</strong></a></td>
+              <td>MoodBoards<br/><a href='https://moodboards-inc.herokuapp.com' target='_blank' rel="noopener noreferrer"><strong className='small-text'>(Demo Site)</strong></a></td>
               <td>Creative project management tool. Users can create projects and idea boards and upload images. Comments and direct messages allow users to interact on projects, and supervisors can approve or reject them, as well.</td>
               <td>Ruby on Rails</td>
               <td>Ruby on Rails, SQLite</td>
-              <td className='no-border-right'><a href='https://github.com/joshlacey/moodboards' target='_blank'><strong>Repo</strong></a></td>
+              <td className='no-border-right'><a href='https://github.com/joshlacey/moodboards' target='_blank' rel="noopener noreferrer"><strong>Repo</strong></a></td>
             </tr>
             <tr>
               <td>Trip Tracker</td>
               <td>Trip planning web app utilizing the Yelp API. Users can create trips with destinations to any city served by Yelp. Users can then categorically filter activities fetched from Yelp and save them to their trips for ease of planning.</td>
               <td>React, JavaScript, jQuery</td>
               <td>Ruby on Rails, SQLite</td>
-              <td className='no-border-right'><a href='https://github.com/dtfreemn/module-4-project-react' target='_blank'><strong>Frontend</strong></a> <a href='https://github.com/dtfreemn/module-4-project-rails' target='_blank'><strong>Backend</strong></a></td>
+              <td className='no-border-right'><a href='https://github.com/dtfreemn/module-4-project-react' target='_blank' rel="noopener noreferrer"><strong>Frontend</strong></a> <a href='https://github.com/dtfreemn/module-4-project-rails' target='_blank' rel="noopener noreferrer"><strong>Backend</strong></a></td>
             </tr>
             <tr>
               <td className='last'>Quora Overflow</td>
               <td className='last'>Crowdsourced question and answer app for users to get the information they need from those who know it best.</td>
               <td className='last'>Vanilla JavaScript</td>
               <td className='last'>Ruby on Rails, Postgres</td>
-              <td className='last no-border-right'><a href='https://github.com/dtfreemn/module3-js-frontend' target='_blank'><strong>Frontend</strong></a> <a href='https://github.com/dtfreemn/module3-rails-backend' target='_blank'><strong>Backend</strong></a></td>
+              <td className='last no-border-right'><a href='https://github.com/dtfreemn/module3-js-frontend' target='_blank' rel="noopener noreferrer"><strong>Frontend</strong></a> <a href='https://github.com/dtfreemn/module3-rails-backend' target='_blank' rel="noopener noreferrer"><strong>Backend</strong></a></td>
             </tr>
           </tbody>
         </table>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Document, Page } from 'react-pdf'
-import Loader from './Loader'
+// import { Document, Page } from 'react-pdf'
+// import Loader from './Loader'
 import '../styles/Resume.css'
 // import resume from '../Tim_Freeman_Resume_2017.pdf'
 
@@ -9,7 +9,50 @@ const Resume = (props) => {
 
   return (
     <div className='main-content'>
+      <a href='https://docs.google.com/document/d/1cQs01ul_uAYG2zCaAkSyJfFAWQCSQsavPFGwanRUfIE/edit?usp=sharing' target='_blank'>
+        <button>See Full Resume</button>
+      </a>
       <div id='resume-container'>
+        <div className='col-1 row-1'><strong>Education</strong></div>
+          <table className='col-1 row-2'>
+            <tbody>
+              <tr>
+                <td>Flatiron School<br/>2017</td>
+                <td>Web Development</td>
+                <td>Learned Ruby on Rails, JavaScript, React, Redux, and various other developer tools.</td>
+              </tr>
+              <tr>
+                <td>National-Louis University<br/>2010</td>
+                <td>Master of Arts in Teaching</td>
+                <td>4.0 GPA</td>
+              </tr>
+              <tr>
+                <td>University of Memphis<br/>2008</td>
+                <td>Bachelor of Arts in History and Spanish</td>
+                <td>3.6 GPA</td>
+              </tr>
+            </tbody>
+          </table>
+        <div className='col-2 row-1'><strong>Work Experience</strong></div>
+          <table className='col-2 row-2'>
+            <tbody>
+              <tr>
+                <td>Democracy Prep Charter High School<br/>2011-17</td>
+                <td className='small-text'>Teacher, Grade Level Leader, Department Leader, Athletics Coach</td>
+                <td className='small-text'>Managed teams of teachers in developing and executing department and grade intiatives. Taught history, PE, and government</td>
+              </tr>
+              <tr>
+                <td>Teach For America<br/>2008-10</td>
+                <td>Teacher, Athletics Coach</td>
+                <td className='small-text'>Taught and created standards-aligned curriculum for intro and intermediate level Spanish</td>
+              </tr>
+              <tr>
+                <td>Ministry of Education<br/>2010-11</td>
+                <td>Auxiliar de Conversación</td>
+                <td className='small-text'>Taught 5th/6th grade math, science, and social studies and managed small-group bilingual tutoring sessions</td>
+              </tr>
+            </tbody>
+          </table>
        {/* <Document className='resume' file={resume} loading={<Loader />}> 
           <Page pageNumber={1}/>
         </Document> */}
