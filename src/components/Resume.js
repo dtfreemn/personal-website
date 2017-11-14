bbe2a5d319f003921c2bafcd027398ea3ebe110e
Tim Freeman <d.timothy.freeman@gmail.com>
2017-11-14 10:49:35 -0500

    working on resume page to make it render faster

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Document, Page } from 'react-pdf'
 import Loader from './Loader'
 import '../styles/Resume.css'
-import resume from '../Tim_Freeman_Resume_2017.pdf'
+// import resume from '../Tim_Freeman_Resume_2017.pdf'
 
 const Resume = (props) => {
 
@@ -10,9 +10,9 @@ const Resume = (props) => {
   return (
     <div className='main-content'>
       <div id='resume-container'>
-        <Document className='resume' file={resume} loading={<Loader />}> 
+       {/* <Document className='resume' file={resume} loading={<Loader />}> 
           <Page pageNumber={1}/>
-        </Document>
+        </Document> */}
       </div>
     </div>
   )
