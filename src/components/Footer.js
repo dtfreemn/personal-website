49e618858820b982ae38f0950d8cba7afbe67131
Tim Freeman <d.timothy.freeman@gmail.com>
2017-11-17 16:36:15 -0500

    added aws hosting to footer description

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = (props) => {
 
   return (
     <div id='footer'>
-      This website was built with React.
+      This website was built with React and is hosted on Amazon Web Services.
     </div>
   )
 }
